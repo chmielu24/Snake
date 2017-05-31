6f8fbeff700073ad7f35e4348d2ddf6db9e638b7
LadyMowilaZeMa18 <dawids42@gmail.com>
2017-05-31 21:18:23 +0200

    Added game manager

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,11 +1,20 @@
 #include "Game.h"
 
+#include <iostream>
+using namespace std;
 
 int main()
 {
 	//Start Aplikacji
-	Game::Instance().Start();
+	//Game::Instance().Start();
 
+
+	int b = 2;
+	int x = 0;
+
+	cout << (x = b);
+
+	cin >> x;
 
 	return 0;
 }
